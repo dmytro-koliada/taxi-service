62fbd2f99b7196dbf2dbebbc86fbf8ab75e5dbf2
Dmytro Koliada <dmitrikstar@gmail.com>
2022-05-27 12:33:09 +0300

    implement readme.md

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# taxi-service
+# *TAXI-SERVICE*
+![](src/main/resources/solutions-taxi-park.jpg)
+
+## This taxi service was created to account for cars and drivers of the fleet, as well as the distribution of which taxi driver can drive.
+### When creating the project, a three-level architecture was used, in particular:
+ - a DAO layer(Data Access Object); 
+ - a service layer(implements business logic);
+ - and a controller layer were implemented.
+### Tools used:
+ - IntelliJ IDEA Ultimate IDEA;
+ - ApacheTomcat Tomcat (v9.0.50);
+ - MySQL and MySQL Workbench MySQL.
+
+### Technologies used:
+- MySQL;
+- JDBC;
+- HTML;
+- Maven Checkstyle Plugin;
+- Apache Maven;
+
+## To start the project, you need:
+ - [install the necessary tools](#Tools used:);
+ - make a fork of the project on GitHub to your repository;
+ - then, using the link, clone the project to your device;
+ - then create a database using the scheme specified in the file;
+ - then run Tomcat with the created database;
+ - then you can run the project in any browser by entering localhost: the port specified during creation (default 8080) and then "/" endpoint request;
+ - when you go to the page, you will be prompted to enter your account by entering your login and password, or create a new one if you have not been registered yet;
+ - after entering the main page you will see the menu;
+ you can follow the link you need;
+![](src/main/resources/img.png)
+## Using the links on the main page you can:
+ 1. Display All Drivers - display all taxi drivers;
+ 2. Display All Cars - display all cars of the taxi fleet;
+ 3. Display All Manufacturers - display all car manufacturers located in the taxi depot;
+ 4. Create new Driver - add a new taxi driver;
+ 5. Create new Car - add a new car to the taxi fleet;
+ 6. Create new Manufacturer - add a new car manufacturer;
+ 7. Add Driver to Car - add information about which driver can drive which car;
+
---
+++ src/main/java/taxi/dao/DriverDaoImpl.java
@@ -0,0 +1,138 @@
+package taxi.dao;
+
+import taxi.exception.DataProcessingException;
+import taxi.lib.Dao;
+import taxi.model.Driver;
+import taxi.util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Dao
+public class DriverDaoImpl implements DriverDao {
+    @Override
+    public Driver create(Driver driver) {
+        String query = "INSERT INTO drivers (login, password, name, license_number) "
+                + "VALUES (?, ?, ?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement createDriverStatement = connection.prepareStatement(query,
+                        Statement.RETURN_GENERATED_KEYS)) {
+            createDriverStatement.setString(1, driver.getLogin());
+            createDriverStatement.setString(2, driver.getPassword());
+            createDriverStatement.setString(3, driver.getName());
+            createDriverStatement.setString(4, driver.getLicenseNumber());
+            createDriverStatement.executeUpdate();
+            ResultSet resultSet = createDriverStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                driver.setId(resultSet.getObject(1, Long.class));
+            }
+            return driver;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't create "
+                    + driver + ". ", e);
+        }
+    }
+
+    @Override
+    public Optional<Driver> get(Long id) {
+        String query = "SELECT * FROM drivers WHERE id = ? AND is_deleted = FALSE";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement getDriverStatement = connection.prepareStatement(query)) {
+            getDriverStatement.setLong(1, id);
+            ResultSet resultSet = getDriverStatement.executeQuery();
+            Driver driver = null;
+            if (resultSet.next()) {
+                driver = parseDriverFromResultSet(resultSet);
+            }
+            return Optional.ofNullable(driver);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't get driver by id " + id, e);
+        }
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        String query = "SELECT * FROM drivers WHERE is_deleted = FALSE";
+        List<Driver> drivers = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement getAllDriversStatement = connection.prepareStatement(query)) {
+            ResultSet resultSet = getAllDriversStatement.executeQuery();
+            while (resultSet.next()) {
+                drivers.add(parseDriverFromResultSet(resultSet));
+            }
+            return drivers;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't get a list of drivers from driversDB.",
+                    e);
+        }
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        String query = "UPDATE drivers "
+                + "SET login = ?, password = ?, name = ?, license_number = ? "
+                + "WHERE id = ? AND is_deleted = FALSE";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement updateDriverStatement
+                        = connection.prepareStatement(query)) {
+            updateDriverStatement.setString(1, driver.getLogin());
+            updateDriverStatement.setString(2, driver.getPassword());
+            updateDriverStatement.setString(3, driver.getName());
+            updateDriverStatement.setString(4, driver.getLicenseNumber());
+            updateDriverStatement.setLong(5, driver.getId());
+            updateDriverStatement.executeUpdate();
+            return driver;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't update "
+                    + driver + " in driversDB.", e);
+        }
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        String query = "UPDATE drivers SET is_deleted = TRUE WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement deleteDriverStatement = connection.prepareStatement(query)) {
+            deleteDriverStatement.setLong(1, id);
+            return deleteDriverStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't delete driver with id " + id, e);
+        }
+    }
+
+    private Driver parseDriverFromResultSet(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getObject("id", Long.class);
+        String login = resultSet.getString("login");
+        String password = resultSet.getString("password");
+        String name = resultSet.getString("name");
+        String licenseNumber = resultSet.getString("license_number");
+        Driver driver = new Driver();
+        driver.setId(id);
+        driver.setLogin(login);
+        driver.setPassword(password);
+        driver.setName(name);
+        driver.setLicenseNumber(licenseNumber);
+        return driver;
+    }
+
+    @Override
+    public Optional<Driver> findByLogin(String login) {
+        String query = "SELECT * "
+                + "FROM drivers "
+                + "WHERE login = ? AND is_deleted = FALSE;";
+        try (Connection connection = ConnectionUtil.getConnection();
+                 PreparedStatement getDriverByLoginStatement = connection.prepareStatement(query)) {
+            getDriverByLoginStatement.setString(1, login);
+            ResultSet resultSet = getDriverByLoginStatement.executeQuery();
+            Driver driver = null;
+            if (resultSet.next()) {
+                driver = parseDriverFromResultSet(resultSet);
+            }
+            return Optional.ofNullable(driver);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't get driver by login " + login, e);
+        }
+    }
+}
